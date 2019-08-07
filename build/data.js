b203b69867e37329a27d617e2d7f68a7d7437e71
王丽红 <201402388@qq.com>
2019-08-07 10:41:17 +0800

    修改首页动图层级

---
+++ build/data.js
@@ -0,0 +1,319 @@
+window.data = [
+    {
+    id: 0,
+    type: "summary"
+},
+{
+    id: 1,
+    type: "img",
+    bgImg: "./File/images/cover.jpg"
+},
+{
+    id: 2,
+    type: "video",
+    video: "./File/video/1.mp4",
+    videoPoster:"./File/images/explain/1.png"
+},
+{
+    id: 3,
+    type: "word",
+    bgImg: "./File/images/bg.jpg",
+    content: "./File/images/link1/word/1.gif"
+},
+{
+    id: 4,
+    type: "wordgame2",
+    answerA: "./File/images/link1/question/A.png",
+    answerB: "./File/images/link1/question/B.png",
+    answer: "B",
+    trueVideo: "./File/images/link1/question/true.mp3",
+    falseVideo: "./File/images/link1/question/false.mp3",
+    errorsrc: './File/images/link1/question/errorword1.gif',
+    truesrc: './File/images/link1/question/successword1.gif'
+},
+{
+    id: 5,
+    type: "sentence",
+    bgImg: "./File/images/link1/sentence/bg.jpg",
+    content: "./File/images/link1/sentence/1.gif"
+},
+{
+    id: 6,
+    type: "sentenceLight",
+    sentence: "Let's go to the zoo.",
+    sentenceTip: [0, 1, 2, 3, 4, 5],
+    times:1750,
+    bgImg: "./File/images/bg.jpg"
+}, {
+    id: 7,
+    type: "sentencegame1",
+    sentenceAtip: "Let's go",
+    sentenceBtip: "to the",
+    answer: "A",
+    sentenceA: 'zoo',
+    sentenceB: 'school',
+    pumkinSucsentence: "Let's go to the zoo.",
+    pumpkinsrc: ['./File/images/link1/question/success1.gif', './File/images/link1/question/error1.gif']
+},
+{
+    id: 8,
+    type: "video",
+    video: "./File/video/2.mp4",
+    videoPoster:"./File/images/explain/2.png"
+},
+{
+    id: 9,
+    type: "word",
+    bgImg: "./File/images/bg.jpg",
+    content: "./File/images/link2/word/1.gif"
+},
+{
+    id: 10,
+    type: "wordgame1",
+    answerA: "./File/images/link2/question/A.png",
+    answerB: "./File/images/link2/question/B.png",
+    answer: "A",
+    trueVideo: "./File/images/link2/question/true.mp3",
+    falseVideo: "./File/images/link2/question/false.mp3",
+},
+{
+    id: 11,
+    type: "sentence",
+    bgImg: "./File/images/link2/sentence/bg.jpg",
+    content: "./File/images/link2/sentence/1.gif"
+},
+{
+    id: 12,
+    type: "sentenceLight",
+    sentence: "It's a tiger.",
+    sentenceTip: [0, 1, 2, 3],
+    times:1200,
+    bgImg: "./File/images/bg.jpg"
+}, {
+    id: 13,
+    type: "sentencegame2",
+    sentenceAtip: "It's",
+    sentenceBtip: "a",
+    sentenceA: 'tiger',
+    sentenceB: 'parrot',
+    pumkinSucsentence: "It's a tiger.",
+    answer: "A",
+    chickensrc: "./File/images/link2/question/success1.gif",
+    chickenErrsasrc: "./File/images/link2/question/error1.gif"
+},
+{
+    id: 14,
+    type: "video",
+    video: "./File/video/3.mp4",
+    videoPoster:"./File/images/explain/3.png"
+},
+{
+    id: 15,
+    type: "word",
+    bgImg: "./File/images/bg.jpg",
+    content: "./File/images/link3/word/1.gif"
+},
+{
+    id: 16,
+    type: "wordgame3",
+    answerA: "./File/images/link3/question/A.png",
+    answerB: "./File/images/link3/question/B.png",
+    answer: "B",
+    trueVideo: "./File/images/link3/question/true.mp3",
+    falseVideo: "./File/images/link3/question/false.mp3",
+},
+{
+    id: 17,
+    type: "sentence",
+    bgImg: "./File/images/link3/sentence/bg.jpg",
+    content: "./File/images/link3/sentence/1.gif"
+},
+{
+    id: 18,
+    type: "sentenceLight",
+    sentence: "It's a monkey.",
+    sentenceTip: [0, 1, 2, 3],
+    times:1300,
+    bgImg: "./File/images/bg.jpg"
+}, {
+    id: 19,
+    type: "sentencegame1",
+    sentenceAtip: "It's ",
+    sentenceBtip: "a",
+    answer: "B",
+    sentenceA: 'snake',
+    sentenceB: 'monkey',
+    pumkinSucsentence: "It's a monkey.",
+    pumpkinsrc: ['./File/images/link3/question/success2.gif', './File/images/link3/question/error2.gif']
+},
+{
+    id: 20,
+    type: "video",
+    video: "./File/video/4.mp4",
+    videoPoster:"./File/images/explain/4.png"
+},
+{
+    id: 21,
+    type: "word",
+    bgImg: "./File/images/bg.jpg",
+    content: "./File/images/link4/word/1.gif"
+},
+{
+    id: 22,
+    type: "wordgame5",
+    answerA: "./File/images/link4/question/A.png",
+    answerB: "./File/images/link4/question/B.png",
+    answer: "B"
+},
+{
+    id: 23,
+    type: "sentence",
+    bgImg: "./File/images/link4/sentence/bg.jpg",
+    content: "./File/images/link4/sentence/1.gif"
+},
+{
+    id: 24,
+    type: "sentenceLight",
+    sentence: "It's a parrot.",
+    sentenceTip: [0, 1, 2, 3],
+    times:1250,
+    bgImg: "./File/images/bg.jpg"
+}, {
+    id: 25,
+    type: "sentencegame2",
+    sentenceAtip: "It's",
+    sentenceBtip: "a",
+    sentenceA: 'tiger',
+    sentenceB: 'parrot',
+    pumkinSucsentence: "It's a parrot.",
+    answer: "B",
+    chickensrc: "./File/images/link4/question/success2.gif",
+    chickenErrsasrc: "./File/images/link4/question/error2.gif"
+},
+{
+    id: 26,
+    type: "video",
+    video: "./File/video/5.mp4",
+    videoPoster:"./File/images/explain/5.png"
+},
+{
+    id: 27,
+    type: "word",
+    bgImg: "./File/images/bg.jpg",
+    content: "./File/images/link5/word/1.gif"
+},
+{
+    id: 28,
+    type: "wordgame4",
+    answerA: "./File/images/link5/question/A.png",
+    answerB: "./File/images/link5/question/B.png",
+    answer: "A"
+},
+{
+    id: 29,
+    type: "sentence",
+    bgImg: "./File/images/link5/sentence/bg.jpg",
+    content: "./File/images/link5/sentence/1.gif"
+},
+{
+    id: 30,
+    type: "sentenceLight",
+    sentence: "It's a crocodile.",
+    sentenceTip: [0, 1, 2, 3],
+    times:1000,
+    bgImg: "./File/images/bg.jpg"
+}, {
+    id: 31,
+    type: "sentencegame1",
+    sentenceAtip: "It's ",
+    sentenceBtip: "a",
+    answer: "A",
+    sentenceA: 'crocodile',
+    sentenceB: 'monkey',
+    pumkinSucsentence: "It's a crocodile.",
+    pumpkinsrc: ['./File/images/link5/question/success3.gif', './File/images/link5/question/error3.gif']
+},
+{
+    id: 32,
+    type: "video",
+    video: "./File/video/6.mp4",
+    videoPoster:"./File/images/explain/6.png"
+},
+{
+    id: 33,
+    type: "word",
+    bgImg: "./File/images/bg.jpg",
+    content: "./File/images/link6/word/1.gif"
+},
+{
+    id: 34,
+    type: "wordgame3",
+    answerA: "./File/images/link6/question/A.png",
+    answerB: "./File/images/link6/question/B.png",
+    answer: "B",
+    trueVideo: "./File/images/link6/question/true.mp3",
+    falseVideo: "./File/images/link6/question/false.mp3",
+},
+{
+    id: 35,
+    type: "sentence",
+    bgImg: "./File/images/link6/sentence/bg.jpg",
+    content: "./File/images/link6/sentence/1.gif"
+},
+{
+    id: 36,
+    type: "sentenceLight",
+    sentence: "It's a snake.",
+    sentenceTip: [0, 1, 2, 3],
+    times:1400,
+    bgImg: "./File/images/bg.jpg"
+}, {
+    id: 37,
+    type: "sentencegame2",
+    sentenceAtip: "It's",
+    sentenceBtip: "a",
+    sentenceA: 'parrot',
+    sentenceB: 'snake',
+    pumkinSucsentence: "It's a snake.",
+    answer: "B",
+    chickensrc: "./File/images/link6/question/success3.gif",
+    chickenErrsasrc: "./File/images/link6/question/error3.gif"
+}, {
+    id: 38,
+    type: "endGame1",
+    T1A: "./File/images/sixQ/T1/A.png",
+    T1B: "./File/images/sixQ/T1/B.png",
+    T2A: "./File/images/sixQ/T2/A.png",
+    T2B: "./File/images/sixQ/T2/B.png",
+    T3A: "./File/images/sixQ/T3/A.png",
+    T3B: "./File/images/sixQ/T3/B.png",
+    T4A: "./File/images/sixQ/T4/A.png",
+    T4B: "./File/images/sixQ/T4/B.png",
+    T5A: "./File/images/sixQ/T5/A.png",
+    T5B: "./File/images/sixQ/T5/B.png",
+    T6A: "./File/images/sixQ/T6/A.png",
+    T6B: "./File/images/sixQ/T6/B.png",
+    answerArr: ["B", "B", "A", "A", "B", "A"]
+
+}, {
+    id: 39,
+    type: "endGame2",
+    T1A: "./File/images/sixQ/T1/A.png",
+    T1B: "./File/images/sixQ/T1/B.png",
+    T2A: "./File/images/sixQ/T2/A.png",
+    T2B: "./File/images/sixQ/T2/B.png",
+    T3A: "./File/images/sixQ/T3/A.png",
+    T3B: "./File/images/sixQ/T3/B.png",
+    T4A: "./File/images/sixQ/T4/A.png",
+    T4B: "./File/images/sixQ/T4/B.png",
+    T5A: "./File/images/sixQ/T5/A.png",
+    T5B: "./File/images/sixQ/T5/B.png",
+    T6A: "./File/images/sixQ/T6/A.png",
+    T6B: "./File/images/sixQ/T6/B.png",
+    answerArr: ["B", "B", "A", "A", "B", "A"]
+
+}, {
+    id: 40,
+    type: "summary"
+}
+]
